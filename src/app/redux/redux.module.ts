e7bff240375a5ae439bc3761d1ebaf51c5b00955
Artur Pietrucha <artur.pietrucha@nexio.pl>
2020-07-02 08:59:05 +0200

    redux - added redux devtools

--- src/app/redux/redux.module.ts
+++ src/app/redux/redux.module.ts
@@ -1,9 +1,9 @@
-import {NgModule} from '@angular/core';
+import {isDevMode, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {NgRedux, NgReduxModule} from "ng2-redux";
+import {DevToolsExtension, NgRedux, NgReduxModule} from "ng2-redux";
 import {INITIAL_STATE, MyAppState, rootReducer} from "./store";
 import {FirstComponentComponent} from './components/first-component/first-component.component';
-import { SecondComponentComponent } from './components/second-component/second-component.component';
+import {SecondComponentComponent} from './components/second-component/second-component.component';
 
 
 @NgModule({
@@ -19,7 +19,8 @@ import { SecondComponentComponent } from './components/second-component/second-c
 })
 export class ReduxModule {
 
-  constructor(ngRedux: NgRedux<MyAppState>) {
-    ngRedux.configureStore(rootReducer, INITIAL_STATE);
+  constructor(ngRedux: NgRedux<MyAppState>, devTools: DevToolsExtension) {
+    let enhancers = isDevMode() ? [devTools.enhancer()] : [];
+    ngRedux.configureStore(rootReducer, INITIAL_STATE, [], enhancers);
   }
 }
